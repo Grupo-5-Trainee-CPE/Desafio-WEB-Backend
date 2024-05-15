c0ec3e75f0ad360c1b61539d61ea42e58ab19490
LauroEmm <lauro.rafael.ismart@gmail.com>
2024-05-15 20:35:18 -0300

    remove console.log

--- src/Controllers/ProjetoController.js
+++ src/Controllers/ProjetoController.js
@@ -1,32 +1,28 @@
 const ProjetoModel = require("../Models/ProjetoModel");
 
 class ProjetoController {
+  async create(req, res) {
+    const projeto = await ProjetoModel.create(req.body);
+    return res.status(200).json(projeto);
+  }
 
-async create (req, res) {
-   console.log (req.body) 
-   const projeto = await ProjetoModel.create(req.body);
-   return res.status(200).json(projeto);
+  async delete(req, res) {
+    const { id } = req.params;
+    await ProjetoModel.findByIdAndDelete(id);
+    return res.status(200).json({ mensagem: "Projeto deletado com sucesso!" });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const projeto = await ProjetoModel.findByIdAndUpdate(id, req.body);
+
+    return res.status(200).json(projeto);
+  }
+
+  async read(req, res) {
+    const projetos = await ProjetoModel.find();
+    return res.status(200).json(projetos);
+  }
 }
-
-async delete (req, res) {
-    const { id } = req.params; 
-    await ProjetoModel.findByIdAndDelete(id); 
-    return res.status(200).json({"mensagem": "Projeto deletado com sucesso!"});
-}
-
-async update (req, res) {
-   const {id} = req.params 
-
-   const projeto = await ProjetoModel.findByIdAndUpdate(id, req.body)
-
-   return res.status(200).json(projeto);
-}
-
-async read (req, res) {
-   const projetos = await ProjetoModel.find();
-   return res.status(200).json(projetos);
-}
-
-
-}
-module.exports = new ProjetoController(); 
+module.exports = new ProjetoController();
